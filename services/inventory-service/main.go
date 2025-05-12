4b7a0e0aca7cadfb3c38beb2cfdd4c78d8770e33
Patryk Zajdel <57092803+xMOROx@users.noreply.github.com>
2025-05-12 06:45:50 +0200

    services: simple apps with logging

--- services/inventory-service/main.go
+++ services/inventory-service/main.go
@@ -3,9 +3,10 @@ package main
 import (
 	"Service-sharing-environment-project/proto/inventory"
 	"Service-sharing-environment-project/services/inventory-service/internal"
-	"google.golang.org/grpc"
 	"log"
 	"net"
+
+	"google.golang.org/grpc"
 )
 
 const port = ":50051"
@@ -13,13 +14,15 @@ const port = ":50051"
 func main() {
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+		log.Fatalf("Inventory Service: failed to listen: %v", err)
 	}
 
 	grpcServer := grpc.NewServer()
+
 	inventory.RegisterInventoryServiceServer(grpcServer, &internal.InventoryServer{})
-	log.Printf("Inventory Service listening on %s", port)
+
+	log.Printf("Inventory Service: Starting gRPC server, listening on %s", port)
 	if err := grpcServer.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
+		log.Fatalf("Inventory Service: failed to serve gRPC: %v", err)
 	}
 }
