dfc6864a23d303bbf6efb3dc5187837be8678402
Patryk Zajdel <57092803+xMOROx@users.noreply.github.com>
2025-05-23 20:23:46 +0200

    scripts: remove prometheus

--- scripts/clean-ports.sh
+++ scripts/clean-ports.sh
@@ -6,25 +6,6 @@ PROJECT_ROOT=$(git rev-parse --show-toplevel)
 # shellcheck disable=SC1091
 source "$PROJECT_ROOT/scripts/utils.sh"
 
-log_step "Finding prometheus process id"
-# Allow pgrep to fail without exiting the script (due to set -e) by using '|| true'
-prometheus_process_id=$(pgrep -f "kubectl port-forward .*prometheus.*$PROMETHEUS_PORT" || true)
-
-if [ -n "$prometheus_process_id" ]; then
-  log_success "Found prometheus process id(s): $prometheus_process_id"
-  log_step "Killing prometheus process(es)"
-  # Unquote variable to allow kill to handle multiple PIDs if pgrep returns them
-  if kill $prometheus_process_id; then
-    log_success "Killed prometheus process id(s): $prometheus_process_id"
-  else
-    log_error "Failed to kill prometheus process id(s): $prometheus_process_id. They might have already exited or you may lack permissions."
-  fi
-else
-  log_info "Prometheus port-forward process not found."
-fi
-
-echo
-
 log_step "Finding grafana process id"
 grafana_process_id=$(pgrep -f "kubectl port-forward .*grafana.*$GRAFANA_PORT" || true)
 
--- scripts/forward-ports.sh
+++ scripts/forward-ports.sh
@@ -6,16 +6,7 @@ PROJECT_ROOT=$(git rev-parse --show-toplevel)
 # shellcheck disable=SC1091
 source "$PROJECT_ROOT/scripts/utils.sh"
 
-PROMETHEUS_PORT=9090
 GRAFANA_PORT=3000
-
-log_step "Finding prometheus server name"
-prometheus_pod=$(kubectl get pods -n default -l app.kubernetes.io/part-of=prometheus -l app.kubernetes.io/component=server -o jsonpath='{.items[*].metadata.name}')
-log_success "Found prometheus pod: $prometheus_pod"
-
-log_step "Forwarding prometheus port"
-kubectl port-forward "${prometheus_pod}" -n default $PROMETHEUS_PORT:$PROMETHEUS_PORT &
-log_success "Forwarding prometheus port to localhost:$PROMETHEUS_PORT"
 
 log_step "Finding grafana pod name"
 grafana_pod=$(kubectl get pods -n default -l app.kubernetes.io/name=grafana -o jsonpath='{.items[*].metadata.name}')
