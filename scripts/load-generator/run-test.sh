d089cac026b65b7990a907a148d2d35e70951878
szymo <szymon.glomski@gmail.com>
2025-06-06 22:19:38 +0200

    MAke possible testing inventory directly

--- scripts/load-generator/run-test.sh
+++ scripts/load-generator/run-test.sh
@@ -12,6 +12,33 @@ if ! kubectl get configmap order-proto --namespace demo >/dev/null 2>&1; then
 	echo "ConfigMap 'order-proto' created."
 fi
 
+uninstall_if_exists grpc-addproduct
+helm install grpc-addproduct "$(dirname "$0")/../../infrastructure/grpc-load-generator" --namespace demo \
+  --set grpc.call=inventory.InventoryService.AddProduct \
+  --set-literal grpc.payload='{"product_id": "P111", "name": "New Product", "description": "Test item", "category": "TestCat", "discontinued": false, "available_quantity": 100, "is_available": true}' \
+  --set grpc.proto="/proto/inventory.proto" \
+  --set grpc.concurrency=10 \
+  --set grpc.requests=100 \
+  --set grpc.target="demo-inventory-service:50051"
+
+uninstall_if_exists grpc-listproducts
+helm install grpc-listproducts "$(dirname "$0")/../../infrastructure/grpc-load-generator" --namespace demo \
+  --set grpc.call=inventory.InventoryService.ListProducts \
+  --set-literal grpc.payload='{"category": "TestCat", "include_discontinued": false}' \
+  --set grpc.proto="/proto/inventory.proto" \
+  --set grpc.concurrency=1 \
+  --set grpc.requests=10 \
+  --set grpc.target="demo-inventory-service:50051"
+
+uninstall_if_exists grpc-removeproduct
+helm install grpc-removeproduct "$(dirname "$0")/../../infrastructure/grpc-load-generator" --namespace demo \
+  --set grpc.call=inventory.InventoryService.RemoveProduct \
+  --set-literal grpc.payload='{"product_id": "P111"}' \
+  --set grpc.proto="/proto/inventory.proto" \
+  --set grpc.concurrency=10 \
+  --set grpc.requests=100 \
+  --set grpc.target="demo-inventory-service:50051"
+
 uninstall_if_exists grpc-checkavailability
 helm install grpc-checkavailability "$(dirname "$0")/../../infrastructure/grpc-load-generator" --namespace demo \
   --set grpc.call=order.OrderService.CheckItemAvailability \
